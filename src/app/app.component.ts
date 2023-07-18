045bded083aa4a99eb4b687cd06d8bb445a007b1
santiarroyave <santi98cv@hotmail.com>
2023-07-18 16:41:22 +0200

    Instalando angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sao-fe-gc-ex01-prueba-tecnica-frontend-pokeapi-07-2023';
+}
