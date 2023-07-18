b0d3c68b536e5e6b4724aae523715c6ee41c9893
santiarroyave <santi98cv@hotmail.com>
2023-07-18 20:00:53 +0200

    Mostrando pokemons

--- src/app/components/lista-pokemons/lista-pokemons.component.ts
+++ src/app/components/lista-pokemons/lista-pokemons.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { ResourceLoader } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { PokemonsService } from 'src/app/services/pokemons.service';
 
 @Component({
   selector: 'app-lista-pokemons',
   templateUrl: './lista-pokemons.component.html',
   styleUrls: ['./lista-pokemons.component.css']
 })
-export class ListaPokemonsComponent {
+export class ListaPokemonsComponent implements OnInit {
 
+  // ATRIBUTOS
+  pokemons:any = new Array;
+  pokemon:any = null;
+  // idsNamesImages:any[] = [];
+  idsNamesImages:any= new Array;
+
+  // CONSTRUCTOR
+  constructor(private pokemonsService: PokemonsService){};
+
+  
+  // METODOS
+  ngOnInit(): void {
+    // Obtiene todos los pokemons y los filtra creando un array con el ID, Nombre y Foto
+    this.pokemonsService.getAll().subscribe( result => {
+      this.obtenerDatosFiltrados(result);
+    });
+  }
+  
+  verPokemon(url:string){
+    alert("Mostrando pokemon");
+  };
+
+  // Recorre la lista de pokemons y guarda (ID, Nombre y Foto en la variable Pokemons)
+  obtenerDatosFiltrados(lista:any):any{
+    let urls:any[] = [];
+
+    // Extrae las urls y las guarda en un array
+    for (let i = 0; i < lista.results.length; i++) {
+      urls.push(lista.results[i].url);
+    }
+
+    // Recorre las urls y obtiene las Ids, Fotos y Nombres
+    for (let i = 0; i < urls.length; i++) {
+      this.pokemonsService.getByUrl(urls[i]).subscribe( result => {
+        this.pokemons.push(this.obtenerIdNombreFoto(result));
+      });
+    }
+  }
+
+  // Recibe un pokemon como argumento, filtra la informacion y devuelve un string con el (ID, Nombre y Foto)
+  obtenerIdNombreFoto(dato:any):string{
+    // Ruta fotos -> /sprites > other > official-artwork > front_default
+    let pokemon:any;
+    pokemon = {
+      "id": dato.id,
+      "name": dato.name,
+      "image": dato.sprites.other["official-artwork"]["front_default"]
+    };
+    return pokemon;
+  }
 }
